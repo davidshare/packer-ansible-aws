df93bc17e8683d7ffd52e254a3873de04d6a5e8a
davidshare <davidessienshare@gmail.com>
2019-04-05 20:44:57 +0100

    feat(playbook): define ansible playbook
    - create a file to extract the ami-id after the ami has been extracted
    - create an nginx configuration
    - setup the start bash script to run packer, ansible and terraform
    - setup a service with systemd

--- start.sh
+++ start.sh
@@ -1,2 +1,25 @@
+#!/bin/bash
+
+# add environment variables
 source ./.env
-packer build packer_template.json
+
+# run build with packer and get the packer output
+packer build ./packer/packer_template.json 2>&1 | tee packer/packer_output.txt
+
+# get the AMI id
+bash packer/ami_extractor.sh
+
+# switch to the terraform directory
+cd ./terraform
+
+# initialize terraform
+terraform init
+
+# run the terraform plan
+terraform plan
+
+# apply the terraform plan
+terraform apply -auto-approve
+
+# remove the files for getting the ami
+rm ../packer/packer_output.txt
