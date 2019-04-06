9fe5ac1df7731f8ff8e6f50f702258db7c627920
davidshare <davidessienshare@gmail.com>
2019-04-06 19:11:44 +0100

    feat(ec2Instance): provision an ec2 instance with terraform
    - move the ami extractor file out of the packer directory
    - move remove the security group definition from the packer playbook
    - create an aws instance with terraform
    - create a security group with terraform
    - create a file to hold the terraform variables
    - add comments to the files

--- ami_extractor.sh
+++ ami_extractor.sh
@@ -1,3 +1,4 @@
+# get the image id from an output file that is created after packer is executed and store it in a file so that terraform can access it to provision the instance
 get_ami_id(){
 	awk 'NF{last=$NF} END{print "variable \x22public_ami\x22 { default = \x22" last  "\x22 }" }' ./packer/packer_output.txt > ./terraform/ami_id.tf
 }
